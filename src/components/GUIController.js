6d889fb3be445ba72a94e76d43e3113fed8261da
Alexander Aguilar <alexanderman2004@yahoo.com>
2021-03-16 00:14:57 -0400

    Added basic mobile UI

--- src/components/GUIController.js
+++ src/components/GUIController.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Viewport from "./Viewport";
 import "./GUIController.css";
-import { Grid } from "semantic-ui-react";
+import { Grid, Sidebar, Segment, Menu } from "semantic-ui-react";
 
 /**
  * Handles switching between both desktop and mobile versions of the
@@ -10,23 +10,77 @@ import { Grid } from "semantic-ui-react";
  * @extends React.Component
  */
 class GUIController extends React.Component {
-  render() {
+  /**
+   * Creates the JSX for the desktop version of the viewport.
+   * @returns {JSX}
+   */
+  createDesktopViewport() {
+    return (
+      <Sidebar.Pushable as={Segment}>
+        <Sidebar as={Menu} inverted direction="top" visible width="very thin">
+          <Menu.Item as="a">Undo</Menu.Item>
+          <Menu.Item as="a">Redo</Menu.Item>
+        </Sidebar>
+        <Viewport onCanvasCreation={this.props.onCanvasCreation} />
+      </Sidebar.Pushable>
+    );
+  }
+
+  /**
+   * Create the desktop version of the UI.
+   * @returns {JSX}
+   */
+  createDesktopGUI() {
     return (
       <Grid celled className={"desktopGrid"}>
         <Grid.Row>
           <Grid.Column width={3}>
-            <h1>Col1</h1>
+            <h1>Left Panel</h1>
           </Grid.Column>
+
           <Grid.Column width={11} style={{ padding: "0" }}>
-            <Viewport onCanvasCreation={this.props.onCanvasCreation} />
+            {this.createDesktopViewport()}
           </Grid.Column>
+
           <Grid.Column width={2}>
-            <h1>Col2</h1>
+            <h1>Right Panel</h1>
           </Grid.Column>
         </Grid.Row>
       </Grid>
     );
-    //return <Viewport onCanvasCreation={this.props.onCanvasCreation} />;
+  }
+
+  /**
+   * Create the mobile version of the UI.
+   * @returns {JSX}
+   */
+  createMobileGUI() {
+    return (
+      <div style={{ height: "100vh" }}>
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar as={Menu} inverted direction="top" visible width="very thin">
+            <Menu.Item as="a">Color Palette</Menu.Item>
+            <Menu.Item as="a">Edit</Menu.Item>
+            <Menu.Item as="a">Camera</Menu.Item>
+            <Menu.Item as="a">Project</Menu.Item>
+          </Sidebar>
+
+          <Sidebar as={Menu} inverted direction="bottom" visible width="thin">
+            <Menu.Item as="a">Add</Menu.Item>
+            <Menu.Item as="a">Remove</Menu.Item>
+            <Menu.Item as="a">Paint</Menu.Item>
+          </Sidebar>
+
+          <Viewport onCanvasCreation={this.props.onCanvasCreation} />
+        </Sidebar.Pushable>
+      </div>
+    );
+  }
+
+  render() {
+    // TODO: Change from arbitrary number
+    const isMobile = window.innerWidth < 768;
+    return isMobile ? this.createMobileGUI() : this.createDesktopGUI();
   }
 }
 
