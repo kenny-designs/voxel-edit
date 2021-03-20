4878f0987af155d4c295df75776b2729d2872484
Alexander Aguilar <alexanderman2004@yahoo.com>
2021-03-19 21:32:51 -0400

    Added Brush.js and added menu with brush options

---
+++ src/components/Brush.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+
+/**
+ * Allows user to switch between basic brush options such as add, remove, and paint.
+ * @extends React.Component
+ */
+class Brush extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeBrush: "add",
+    };
+  }
+
+  /**
+   * Handler for each brush option. Upon click, updates the currently
+   * selected brush.
+   * @param {Event} e - React's original SyntheticEvent
+   * @param {data} props - Prop data from the Menu.Item
+   */
+  handleBrushClick = (e, { name }) => {
+    this.setState({ activeBrush: name });
+  };
+
+  render() {
+    const { activeBrush } = this.state;
+
+    return (
+      <React.Fragment>
+        <Menu.Item
+          name="add"
+          active={activeBrush === "add"}
+          onClick={this.handleBrushClick}
+        >
+          Add
+        </Menu.Item>
+        <Menu.Item
+          name="remove"
+          active={activeBrush === "remove"}
+          onClick={this.handleBrushClick}
+        >
+          Remove
+        </Menu.Item>
+        <Menu.Item
+          name="paint"
+          active={activeBrush === "paint"}
+          onClick={this.handleBrushClick}
+        >
+          Paint
+        </Menu.Item>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Brush;
