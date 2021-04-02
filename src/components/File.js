18e6582cd1f56865a48dcb978f63c993fe720c3a
Alexander Aguilar <alexanderman2004@yahoo.com>
2021-04-02 16:07:08 -0400

    Added documentation to File.js

--- src/components/File.js
+++ src/components/File.js
@@ -7,24 +7,31 @@ import FileSaver from "file-saver";
  * project or load one.
  * @TODO File is already a class used within the browser. Rename this! It might
  * be time to start referring to all components as just SomethingComponent or similar.
+ *
+ * @property {Input} loadFileInput - Input used for selecting project file to load
+ * @property {FileReader} loadFileReader - Reads data from selected project file
  * @extends React.Component
  */
 class File extends React.Component {
   constructor(props) {
     super(props);
 
+    // Create input for allowing users to select their project to load
     this.loadFileInput = document.createElement("input");
     this.loadFileInput.type = "file";
     this.loadFileInput.accept = ".json";
     this.loadFileInput.addEventListener("change", this.handleFileSelected);
 
+    // Create FileReader for loading user projects
     this.loadFileReader = new FileReader();
     this.loadFileReader.addEventListener("load", this.handleFileRead);
   }
 
+  /**
+   * Callback for when users wish to save their project. Creates a JSON file
+   * with the contents of the 3D scene then saves locally to the user's device.
+   */
   onSaveProject = () => {
-    console.log("Saving project...");
-
     // Test object for json:
     let myObj = {
       shoppingList: {
@@ -46,14 +53,25 @@ class File extends React.Component {
     FileSaver.saveAs(blob, "shopping-list.json");
   };
 
+  /**
+   * Callback for when users wish to load a pre-existing project.
+   */
   onLoadProject = () => {
     this.loadFileInput.click();
   };
 
+  /**
+   * Handler for when the users selects the file they wish to load.
+   * @param {Event} e
+   */
   handleFileSelected = (e) => {
     this.loadFileReader.readAsText(this.loadFileInput.files[0]);
   };
 
+  /**
+   * Handler for reading the data from the user's selected project file.
+   * @param {Event} e
+   */
   handleFileRead = (e) => {
     const json = JSON.parse(e.target.result);
     console.log(json);
