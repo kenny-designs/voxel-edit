e05d02c62eddf49d136abafb95b42933d791a577
Alexander Aguilar <alexanderman2004@yahoo.com>
2021-04-04 12:05:28 -0400

    Added handler onExportImage

--- src/components/Render.js
+++ src/components/Render.js
@@ -2,11 +2,17 @@ import React from "react";
 import { Dropdown } from "semantic-ui-react";
 
 class Render extends React.Component {
+  onExportImage = () => {
+    console.log("Now exporting...");
+  };
+
   render() {
     return (
       <Dropdown text="Render" pointing className="link item">
         <Dropdown.Menu>
-          <Dropdown.Item>Export Scene to Image</Dropdown.Item>
+          <Dropdown.Item onClick={this.onExportImage}>
+            Export Scene to Image
+          </Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
     );
